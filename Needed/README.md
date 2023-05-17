626bf4ede39e05198815a71a9fea2a59e6529cf0
Ebony Ford <marketingwithebony@gmail.com>
2023-05-16 22:44:44 -0400

    created social API

---
+++ Needed/README.md
@@ -0,0 +1,24 @@
+# mongo-social-api
+
+USE THE SOLVED FOLDER AND REPLICATE IT 
+
+share thoughts (create and delete)
+read all thoughts (retrieve)
+be able to comment thoughts on additional posts (update? create?)
+
+create a friends list (create, retreive, update, delete)
+
+Project Name: AntiSocial
+
+Project Description:
+It's the new way to social - online! Antisocial uses mongodb and express to add users and different routes that will allow the user to be added to the database and make their first thought post. If you are a user on the site you can add reactions to other user's posts. As a user you can retrieve, delete and update posts and reactions to posts. Be interactive and share your thoughts!
+
+Installation:
+From the command line run 'npm install' and 'npm start'
+
+Use mongodb to see the data populate there and use insomnia to test your routes based on the endpoints given in the server js file.
+
+Resources:
+Class Modules and ChatGPT
+
+Repository: 
---
+++ Needed/server.js
@@ -0,0 +1,230 @@
+//use express, mongodb, and establish the port
+
+const express = require('express');
+const { MongoClient } = require('mongodb');
+
+const app = express();
+const port = 3001;
+
+const connectionStringURI = `mongodb://127.0.0.1:27017`;
+
+const client = new MongoClient(connectionStringURI);
+
+let db;
+
+const dbName = 'userListDB';
+
+//when connected will not create multiples
+
+client.connect()
+  .then(() => {
+    console.log('Connected successfully to MongoDB');
+    db = client.db(dbName);
+    // Drops any documents, if they exist
+    db.collection('userList').deleteMany({});
+    // Adds data to database
+    db.collection('userList').insertMany(data)
+      .then(res => console.log(res))
+      .catch(err => {
+        if (err) return console.log(err);
+      });
+
+    app.listen(port, () => {
+      console.log(`Example app listening at http://localhost:${port}`);
+    });
+  })
+  .catch((err) => {
+    console.error('Mongo connection error: ', err.message);
+  });
+
+// Data for document
+const data = [
+  {
+    user: 'BabyPink',
+    // One-to-many relationship
+    // Each book has multiple authors
+    thoughts: [
+      { thought: 'The rain ruined my bday.', featured: true },
+      { thought: 'Pink is better than blue.', featured: true },
+    ],
+    reactions: [
+      {reaction: 'I would have come to your part even in the rain, sorry I missed it.'}
+    
+    ]
+  },
+  {
+    user: 'GossipGirl',
+    thoughts: [
+      { thought: 'What are the moves tonight?', featured: true },
+    
+    ],
+    reactions: [
+      {reaction: 'Girl, go to bed.'}
+    
+    ]
+  },
+  {
+    user: 'HubbaBubba',
+    thoughts: [
+      { thought: 'I loved the new spiderman movie.', featured: true },
+      { thought: 'My gf is the best.', featured: true },
+    ],
+    reactions: [
+      {reaction: 'Loved seeting it with you!'}
+    
+    ]
+  },
+];
+
+app.use(express.json());
+
+//ROUTES
+
+//GET route
+
+app.get('/user-thoughts', (req, res) => {
+  db.collection('userList')
+    // If you do not know the array index, use dot notation to access fields nested in arrays
+    .find({ 'thoughts.featured': true })
+    .toArray()
+    .then(results => res.send(results))
+    .catch(err => {
+      if (err) throw err;
+    });
+});
+
+//POST Route
+
+app.post('/new-user-thoughts', (req, res) => {
+  const { user, thought, featured } = req.body;
+
+  if (!user || !thought || featured === undefined) {
+    return res.status(400).json({ error: 'Invalid request body' });
+  }
+
+  db.collection('userList')
+    .findOneAndUpdate(
+      { user: user },
+      { $push: { thoughts: { thought: thought, featured: featured } } },
+      { returnOriginal: false }
+    )
+    .then(updatedDocument => {
+      if (updatedDocument) {
+        res.status(201).json(updatedDocument.value);
+      } else {
+        res.status(404).json({ error: 'User not found' });
+      }
+    })
+    .catch(err => {
+      console.log('Error updating document: ', err);
+      res.status(500).json({ error: 'Something went wrong' });
+    });
+});
+
+//Delete route
+
+
+app.delete('/user-thoughts/:user', (req, res) => {
+  const user = req.params.user;
+
+  db.collection('userList')
+    .findOneAndUpdate(
+      { user },
+      { $unset: { thoughts: '' } },
+      { returnOriginal: false }
+    )
+    .then(updatedDocument => {
+      if (updatedDocument) {
+        res.status(200).json(updatedDocument.value);
+      } else {
+        res.status(404).json({ error: 'User not found' });
+      }
+    })
+    .catch(err => {
+      console.log('Error updating document: ', err);
+      res.status(500).json({ error: 'Something went wrong' });
+    });
+});
+
+//PUT/Update Route to specifc user
+
+app.put('/update-user-thoughts/:user', (req, res) => {
+  const user = req.params.user;
+  const { thought, featured } = req.body;
+
+  if (!thought || featured === undefined) {
+    return res.status(400).json({ error: 'Invalid request body' });
+  }
+
+  db.collection('userList')
+    .findOneAndUpdate(
+      { user },
+      { $push: { thoughts: { thought, featured } } },
+      { returnOriginal: false }
+    )
+    .then(updatedDocument => {
+      if (updatedDocument) {
+        res.status(200).json(updatedDocument.value);
+      } else {
+        res.status(404).json({ error: 'User not found' });
+      }
+    })
+    .catch(err => {
+      console.log('Error updating document: ', err);
+      res.status(500).json({ error: 'Something went wrong' });
+    });
+});
+
+//add reaction
+
+app.post('/user-reactions/:user', (req, res) => {
+  const user = req.params.user;
+  const { reaction } = req.body;
+
+  if (!reaction) {
+    return res.status(400).json({ error: 'Invalid request body' });
+  }
+
+  db.collection('userList')
+    .findOneAndUpdate(
+      { user },
+      { $push: { reactions: { reaction } } },
+      { returnOriginal: false }
+    )
+    .then(updatedDocument => {
+      if (updatedDocument) {
+        res.status(201).json(updatedDocument.value);
+      } else {
+        res.status(404).json({ error: 'User not found' });
+      }
+    })
+    .catch(err => {
+      console.log('Error updating document: ', err);
+      res.status(500).json({ error: 'Something went wrong' });
+    });
+});
+
+//delete reaction
+
+app.delete('/user-reactions/:user/:reactionId', (req, res) => {
+  const user = req.params.user;
+  const reactionId = req.params.reactionId;
+
+  db.collection('userList')
+    .findOneAndUpdate(
+      { user },
+      { $pull: { reactions: { _id: reactionId } } },
+      { returnOriginal: false }
+    )
+    .then(updatedDocument => {
+      if (updatedDocument) {
+        res.status(200).json(updatedDocument.value);
+      } else {
+        res.status(404).json({ error: 'User or reaction not found' });
+      }
+    })
+    .catch(err => {
+      console.log('Error updating document: ', err);
+      res.status(500).json({ error: 'Something went wrong' });
+    });
+});
